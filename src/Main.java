39eaebf5530f9aa58b90ce025dd775d9738a6b5a
AlekseySamokhvalov <6560@mail.ru>
2023-06-29 09:50:55 +0300

    Решение задачи 1. Метод, который получает в качестве параметра год, проверяет, является ли он високосным, и выводит результат в консоль.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void checkLeapYear(int year) {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
+    public static void main(String[] args) {
+        int year = 2023;
+        checkLeapYear(year);
     }
 }
